7c4538ef448f1c65cf58997df48f4951df6f8fb1
Ciprien Bossut <chefchino@live.com>
2019-11-26 15:16:03 -0500

    starting inquire work on logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,11 +17,40 @@ function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         // for (var i = 0; i < res.length; i++){
-        // console.log(res[i].item_id + "\nProduct: " + res[i].product + "\nDepartment: " +
-        // res[i].department + "\nPrice: " + res[i].price + "\nOn-Hand: " + res[i].in_stock + 
-        // "\n----------------------------------------\n");
+        // console.log(res[i].item_id + "\nProduct " + res[i].product + "\nDepartment: " +
+        // res[i].department + "\nPrice: " + res[i].price + "\nOn-Hand: " + res[i].in_stock); 
+        // // "\n----------------------------------------\n");
         console.log(res);
         // }
-        connection.end();
+        buyStuff();
+        // connection.end()
     });
 }
+function buyStuff() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        inquire
+        .prompt({
+            name:"id",
+            type: "input",
+            message: "Please enter the ID number of the product you wish to buy."
+        },
+        {
+            name:"amount",
+            type: "input",
+            message: "How many do you want?"
+        })
+        .then(function(answer) {
+            var chosenItem;
+            for(var i = 0; i < res.length; i ++) {
+                if (res[i].item_id === answer.id) {
+                    chosenItem = res[i];
+                }
+            }
+            // if(answer.amount <= res[i].in_stock){
+                
+                // }
+                console.log({chosenItem});
+            })
+        })
+        }
