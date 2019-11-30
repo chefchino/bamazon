43f739355d468cfc6b788cdd6c18dd9d5c44831e
Ciprien Bossut <chefchino@live.com>
2019-11-30 11:30:25 -0500

    part 1 works just need to work on display

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,7 +30,8 @@ function buyStuff() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
         inquire
-        .prompt({
+        .prompt([
+            {
             name:"id",
             type: "input",
             message: "Please enter the ID number of the product you wish to buy."
@@ -39,18 +40,38 @@ function buyStuff() {
             name:"amount",
             type: "input",
             message: "How many do you want?"
-        })
+        }
+        ])
         .then(function(answer) {
             var chosenItem;
+
             for(var i = 0; i < res.length; i ++) {
-                if (res[i].item_id === answer.id) {
+                if (res[i].item_id === parseInt(answer.id)) {
                     chosenItem = res[i];
+                    console.log("chosenItem",chosenItem);
                 }
             }
+                if (chosenItem.in_stock >= parseInt(answer.amount)) {
+                    var sum = chosenItem.price * answer.amount;
+                    chosenItem.in_stock = chosenItem.in_stock - parseInt(answer.amount)
+                    connection.query("UPDATE products SET ? WHERE ?",
+                                      [{in_stock: chosenItem.in_stock},
+                                        {item_id: chosenItem.item_id}],
+                                      function(err) {
+                                          if (err) throw err;
+                                      })
+                    console.log("You made a purchase!" + "\nThere are " + chosenItem.in_stock + " " + chosenItem.product + " left.")
+                    console.log("Your total is $" + sum);
+                    buyStuff();
+                    
+                }else{
+                    console.log("Insufficient Quantity");
+                    buyStuff();
+                }
+            
+            
             // if(answer.amount <= res[i].in_stock){
                 
-                // }
-                console.log({chosenItem});
             })
         })
-        }
+    }
