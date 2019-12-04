9a1d7ad3339ede1788184b39121f10c0842f3ad5
Ciprien Bossut <chefchino@live.com>
2019-12-03 20:13:36 -0500

    table for sales view

--- supervisorBamazon.js
+++ supervisorBamazon.js
@@ -12,4 +12,37 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    afterConnection();
 });
+function afterConnection() {
+    inquire.prompt(
+        {
+            name: "menu",
+            type: "list",
+            choices: ["View Product Sales By Department", "Create new Department"],
+            message: "What do you want to do?"
+        })
+        .then(function (answer) {
+            switch(answer.menu) {
+                case "View Product Sales By Department":
+                    viewSales();
+                    break;
+                case "Create new Department":
+                    create();
+                    break;
+                default:
+                    console.log("Try Again");
+                    afterConnection();
+            }
+        })
+}
+function viewSales() {
+    // var query = "SELECT products.department, departments.department_name, products.product_sales FROM products INNER JOIN departments ON (product.department = departments.department_name AND products.product_sales = departments.product_sales) WHERE (departments.department = ? AND products.department = ?"
+    connection.query("SELECT * FROM departments", function(err, res) {
+        if (err) throw err;
+        // for (var i = 0; i < res.length; i++) {
+            console.table(res)
+        // }
+        afterConnection();
+    })
+}
